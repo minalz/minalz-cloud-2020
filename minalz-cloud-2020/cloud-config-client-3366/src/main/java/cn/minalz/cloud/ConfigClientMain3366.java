db2f68cf6fbc6cb8132696a5f19dea7bf4d268b3
minalz <119687281@qq.com>
2020-06-13 21:51:20 +0800

    add Bus module

---
+++ minalz-cloud-2020/cloud-config-client-3366/src/main/java/cn/minalz/cloud/ConfigClientMain3366.java
@@ -0,0 +1,19 @@
+package cn.minalz.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @description:
+ * @author: minalz
+ * @date: 2020-06-13 00:17
+ **/
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3366 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3366.class, args);
+    }
+}
---
+++ minalz-cloud-2020/cloud-config-client-3366/src/main/java/cn/minalz/cloud/controller/ConfigClientController.java
@@ -0,0 +1,29 @@
+package cn.minalz.cloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @description:
+ * @author: minalz
+ * @date: 2020-06-13 00:19
+ **/
+@RestController
+@RefreshScope
+public class ConfigClientController
+{
+    @Value("${server.port}")
+    private String serverPort;
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String configInfo()
+    {
+        return "serverPort: "+serverPort+"\t\n\n configInfo: "+configInfo;
+    }
+
+}
